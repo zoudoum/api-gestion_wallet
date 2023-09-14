c11323af06ca5733ea9507978baffe0ace452e64
unknown <habibjeux@gmail.com>
2023-09-14 15:00:46 +0000

    Ajout des services suivantes : sauvegarde et consultaion des comptes

--- src/main/java/m1sir/groupe1/applicationwallet/services/CompteService.java
+++ src/main/java/m1sir/groupe1/applicationwallet/services/CompteService.java
@@ -3,19 +3,17 @@ package m1sir.groupe1.applicationwallet.services;
 import m1sir.groupe1.applicationwallet.entite.Client;
 import m1sir.groupe1.applicationwallet.entite.Compte;
 import m1sir.groupe1.applicationwallet.repository.CompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class CompteService {
+    @Autowired
     private CompteRepository compteRepository;
+    @Autowired
     private  ClientService clientService;
-
-    public CompteService(CompteRepository compteRepository, ClientService clientService) {
-        this.compteRepository = compteRepository;
-        this.clientService = clientService;
-    }
 
     public void creer(Compte compte) {
         Client client=this.clientService.lireOUCreer(compte.getClient());
@@ -30,5 +28,12 @@ public class CompteService {
             return  optionalCompte.get();
         }
         return  null;
+    }
+
+    public void sauvegarder(Compte compte) {
+        compteRepository.save(compte);
+    }
+    public Iterable<Compte> lireComptes() {
+        return compteRepository.findAll();
     }
 }
