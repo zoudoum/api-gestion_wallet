32da2a2a42aee3bfdb6cd848f191e599224cf80d
habibjeux <habibjeux@gmail.com>
2023-09-14 15:04:57 +0000

    Ajout des fontionnalitéssuivantes : s dépot et retrait sur un compte, consultation des comptes, consultation d'un compte avec un id

--- src/main/java/m1sir/groupe1/applicationwallet/controller/CompteController.java
+++ src/main/java/m1sir/groupe1/applicationwallet/controller/CompteController.java
@@ -4,6 +4,7 @@ import m1sir.groupe1.applicationwallet.entite.Client;
 import m1sir.groupe1.applicationwallet.entite.Compte;
 import m1sir.groupe1.applicationwallet.services.ClientService;
 import m1sir.groupe1.applicationwallet.services.CompteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +13,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "compte")
 public class CompteController {
+    @Autowired
     private CompteService compteService;
+    @Autowired
     private ClientService clientService;
 
-    public CompteController(CompteService compteService,ClientService clientService) {
-        this.compteService = compteService;
-        this.clientService=clientService;
-    }
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public  void creer(@RequestBody Compte compte){
         this.compteService.creer(compte);
 
     }
-    @GetMapping(path = "/solde/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "compte/solde/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, Double> consultationSolde(@PathVariable int id) {
         Compte compte = this.compteService.lire(id);
 
@@ -35,5 +33,43 @@ public class CompteController {
         response.put("solde", compte.getSolde());
 
         return response;
+    }
+
+    @PutMapping("compte/depot/{id}")
+    public Map<String, Double> depot(@PathVariable int id, @RequestBody Compte nouveauCompte) {
+        Compte compte = this.compteService.lire(id);
+        Double ancienSolde = compte.getSolde();
+        Double soldeDepot = nouveauCompte.getSolde();
+
+        compte.setSolde(ancienSolde + soldeDepot);
+
+        Map<String, Double> response = new HashMap<>();
+        compteService.sauvegarder(compte);
+        response.put("nouveau_solde", compte.getSolde());
+        return response;
+    }
+    @PutMapping("compte/retrait/{id}")
+    public Map<String, Double> retrait(@PathVariable int id, @RequestBody Compte nouveauCompte) {
+        Compte compte = this.compteService.lire(id);
+        Double ancienSolde = compte.getSolde();
+        Double soldeRetrait = nouveauCompte.getSolde();
+        Map<String, Double> response = new HashMap<>();
+
+        if(ancienSolde >= soldeRetrait) {
+            compte.setSolde(ancienSolde - soldeRetrait);
+            compteService.sauvegarder(compte);
+            response.put("nouveau_solde", compte.getSolde());
+        }
+        else
+            return null;
+        return response;
+    }
+        @GetMapping("compte/{id}")
+    public Compte lireComptes(@PathVariable("id") final int id) {
+        return compteService.lire(id);
+    }
+    @GetMapping("comptes")
+    public Iterable<Compte> lireComptes() {
+        return compteService.lireComptes();
     }
 }
