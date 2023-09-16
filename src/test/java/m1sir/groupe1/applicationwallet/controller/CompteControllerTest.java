0ab843982641bfff9e259810a0487f946090cecc
habibjeux <habibjeux@gmail.com>
2023-09-16 13:44:15 +0000

    initialisation test controlleur Compte

---
+++ src/test/java/m1sir/groupe1/applicationwallet/controller/CompteControllerTest.java
@@ -0,0 +1,39 @@
+package m1sir.groupe1.applicationwallet.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+class CompteControllerTest {
+
+    @Test
+    void creer() {
+    }
+
+    @Test
+    void consultationSolde() {
+    }
+
+    @Test
+    void depot() {
+    }
+
+    @Test
+    void retrait() {
+    }
+
+    @Test
+    void lireComptes() {
+    }
+
+    @Test
+    void testLireComptes() {
+    }
+}
